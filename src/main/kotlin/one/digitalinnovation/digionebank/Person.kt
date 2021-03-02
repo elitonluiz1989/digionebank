49a824b336dcc295a07cb4f43b889c267cc84f21
Eliton Santos <elitonluiz1989@gmail.com>
2021-03-01 21:19:38 -0400

    Adding function to get the person data

--- src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -4,12 +4,17 @@ class Person {
     lateinit var name:String
     lateinit var cpf:String
     private set
+
+    constructor() {
+        cpf = "123.456.789-10"
+    }
+
+    fun getInfo() = "$name, $cpf";
 }
 
 fun main() {
     val person = Person()
     person.name = "Pedro"
 
-    println(person.name)
-    println(person.cpf)
+    println(person.getInfo())
 }
