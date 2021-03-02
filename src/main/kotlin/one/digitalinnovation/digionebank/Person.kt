5026748bf83f6440ec75901809b97d9906a56120
Eliton Santos <elitonluiz1989@gmail.com>
2021-03-01 21:10:35 -0400

    Adding private visibility modifier at a property

--- src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Person.kt
@@ -3,12 +3,12 @@ package one.digitalinnovation.digionebank
 class Person {
     lateinit var name:String
     lateinit var cpf:String
+    private set
 }
 
 fun main() {
     val person = Person()
-    person.name = "Pedro";
-    person.cpf = "123.456.789-10";
+    person.name = "Pedro"
 
     println(person.name)
     println(person.cpf)
